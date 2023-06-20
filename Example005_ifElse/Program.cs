a41b6f87ae3b4adec57041a2106520a8c07596f9
Usmanov Saut <cayt99@mail.ru>
2023-06-21 00:08:06 +0600

    Add example005

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write ("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username. ToLower() == "saut")
+
+{
+    Console.WriteLine("Ура это же Саут!");
+}
+else 
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
