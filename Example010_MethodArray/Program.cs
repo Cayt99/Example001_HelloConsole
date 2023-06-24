0fbc0bcf39db981b7f37794af6d9b6ce13610b8a
Usmanov Saut <cayt99@mail.ru>
2023-06-24 16:21:38 +0600

    Add Example 010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 71, 62, 13, 43, 225, 13, 56, 57, 87, 96 };
+
+int n = array.Length;
+int find = 13;
+
+int index = 0;
+
+while(index < n) 
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+    
+    
